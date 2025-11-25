6907b8413c88bbb9cc41ae336d96004a9379f2a3
choihyunjii <such1123@naver.com>
2025-11-25 17:44:39 +0900

    로그인 테스트

--- src/features/login/component/LoginButton.jsx
+++ src/features/login/component/LoginButton.jsx
@@ -1,14 +1,19 @@
 import googleLogo from "../../../assets/logo/googleLogo.png";
 import "../css/LoginButton.css";
+
 const LoginButton = () => {
   const handleGoogleLogin = () => {
-    window.location.href = "https://datecast.site/oauth2/authorization/google";
+    window.location.href = `${
+      import.meta.env.VITE_API_URL
+    }/oauth2/authorization/google`;
   };
+
   return (
     <button className="LoginButton" onClick={handleGoogleLogin}>
-      <img src={googleLogo} />
-      <h4> 구글로 시작하기</h4>
+      <img src={googleLogo} alt="구글 로그인" />
+      <h4>구글로 시작하기</h4>
     </button>
   );
 };
+
 export default LoginButton;
--- src/shared/hooks/useLogout.js
+++ src/shared/hooks/useLogout.js
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation,useQueryClient } from "@tanstack/react-query";
 import { logoutRequest } from "../../networks/apis/authApi";
 
 export const useLogout = () => {
