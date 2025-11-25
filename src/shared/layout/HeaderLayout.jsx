18aac4998c88a49f0a6a30ff3b293dc5088418dc
choihyunjii <such1123@naver.com>
2025-11-25 15:58:53 +0900

    로그인시 토큰 콘솔에 나오게

--- src/shared/layout/HeaderLayout.jsx
+++ src/shared/layout/HeaderLayout.jsx
@@ -14,6 +14,7 @@ const HeaderLayout = ({ children }) => {
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     if (token) {
+      console.log("localStorage token:", localStorage.getItem("accessToken"));
       setIsLoggedIn(true); // 로그인 상태로 변경
     } else {
       setIsLoggedIn(false);
